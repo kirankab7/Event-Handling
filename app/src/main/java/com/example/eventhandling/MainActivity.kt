0183abeb28874e01be35783409629e7ba67088fd
kirankab7 <kirankab7@gmail.com>
2024-06-11 19:50:37 +0530

    Simple App

---
+++ app/src/main/java/com/example/eventhandling/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.eventhandling
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.eventhandling.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.button.setOnClickListener {
+            binding.textview.text = "Button clicked"
+        }
+
+    }
+}
